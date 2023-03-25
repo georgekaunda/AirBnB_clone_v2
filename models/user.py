d31b31cec10235266850ef65004a818a46056ac8
SamLong01 <samueladebayo1015@gmail.com>
2023-03-25 20:43:01 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+
+# SQLAlchemy modules
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
     """
-    __tablename__ = "users"
+    Defines a class User
+    Attributes:
+        __tablename__ (str): Users MySQL table name
+        email (String): User's email address column
+        password (String): User's password column
+        first_name (String): User's first name column
+        last_name (String): User's last name column
+    """
+    __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+
+    places = relationship('Place',
+                          backref='user',
+                          cascade='all, delete-orphan',
+                          passive_deletes=True)
+    reviews = relationship('Review',
+                           backref='user',
+                           cascade='all, delete-orphan',
+                           passive_deletes=True)
